d915c2c4d1f66c48380f09bf5b203e24e0f85d8f
Patrick Metzdorf <patrick.metzdorf@hey.com>
2024-07-22 23:08:44 +0100

    Update with posthog analytics

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -5,12 +5,27 @@
  */
 
 import { RemixBrowser } from "@remix-run/react";
-import { startTransition } from "react";
+import { startTransition, useEffect } from "react";
 import { hydrateRoot } from "react-dom/client";
+import posthog from 'posthog-js';
+
+function PosthogInit() {
+  useEffect(() => {
+    posthog.init('phc_Fdzlm8xerItmC8dlIam0qQ59QdCDWfdC7aBUw5aReqa', {
+      api_host: 'https://eu.i.posthog.com',
+      person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+    });
+  }, []);
+
+  return null;
+}
 
 startTransition(() => {
   hydrateRoot(
     document,
-    <RemixBrowser />
+    <>
+      <RemixBrowser />
+      <PosthogInit />
+    </>
   );
 });
